a42338892658aabfd8134e670497003c40c5d241
Diego Martín Campos <campos.martin.diego@gmail.com>
2020-11-05 20:44:38 +0100

    struct nodo y añadir encuesta

--- mi_codigo/2020-11-5_punteros_y_ficheros/main.cpp
+++ mi_codigo/2020-11-5_punteros_y_ficheros/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+struct nodo{
+    String nombre;
+    string apellidos;
+    string email;
+    string git;
+    bool repite;
+    bool act1;
+    bool exponer;
+    int exp;
+    int nivel;
+    int ano;
+    int mes;
+    int presencial;
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
