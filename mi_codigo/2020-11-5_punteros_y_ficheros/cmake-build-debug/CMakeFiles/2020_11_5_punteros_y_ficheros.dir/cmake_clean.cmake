319d556704f78c6863eec62afe40019c5fc19904
Diego Martín Campos <campos.martin.diego@gmail.com>
2020-11-05 20:35:10 +0100

    crear

---
+++ mi_codigo/2020-11-5_punteros_y_ficheros/cmake-build-debug/CMakeFiles/2020_11_5_punteros_y_ficheros.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2020_11_5_punteros_y_ficheros.exe"
+  "2020_11_5_punteros_y_ficheros.exe.manifest"
+  "2020_11_5_punteros_y_ficheros.pdb"
+  "CMakeFiles/2020_11_5_punteros_y_ficheros.dir/main.cpp.obj"
+  "lib2020_11_5_punteros_y_ficheros.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2020_11_5_punteros_y_ficheros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
