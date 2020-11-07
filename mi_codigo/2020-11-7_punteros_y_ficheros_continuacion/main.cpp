62bbd620fa5391d8751a00a1dd8418e080cb8439
Diego Martín Campos <campos.martin.diego@gmail.com>
2020-11-07 02:06:48 +0100

    probamos con 3 listas pero no funciona ni es útil

--- mi_codigo/2020-11-7_punteros_y_ficheros_continuacion/main.cpp
+++ mi_codigo/2020-11-7_punteros_y_ficheros_continuacion/main.cpp
@@ -23,10 +23,22 @@ struct nodo{
 };
 
 nodo *head=NULL, *final=NULL;
+
+int recogerMes();
+int recogerAno();
+void leerFichero();
 int * recogerHora();
 void leerFichero();
 int menu();
-void masEdad(nodo *pNodo);
+void masEdad(int mes, int ano, nodo *head);
+void cumple(int mes, nodo *head);
+void expImpar(nodo *head);
+void expPar(nodo *head);
+void entrega(nodo *head);
+void presencialidad(nodo *head);
+
+
+void presencialidad(nodo *head);
 
 int recogerMes(){
     time_t t = time(NULL);
@@ -158,6 +170,56 @@ void entrega(nodo *head){
     float porcentage = entrega/nAlumnos;
     cout<<">Han entregado la actividad un "<< entrega*100/nAlumnos <<"% de la clase"<<endl;
 }
+void eQvirtual(nodo *headHyflex, nodo *finalHyflex, nodo *aux){
+    if(headHyflex==NULL && finalHyflex==NULL){
+        headHyflex=aux;
+        finalHyflex=aux;
+    }
+    else{
+        finalHyflex->sgt=aux;
+        finalHyflex=aux;
+    }
+}
+void eQpresencial(nodo *headPresencial, nodo *finalPresencial, nodo *aux){
+    if(headPresencial==NULL && finalPresencial==NULL){
+        headPresencial=aux;
+        finalPresencial=aux;
+    }
+    else{
+        finalPresencial->sgt=aux;
+        finalPresencial=aux;
+    }
+}
+/*
+ * IMPORTANTE:Para almacenar un listado de los asistentes presenciales y virtuales y mostrarlos más a delante
+ * hacemos dos listas donde los almacenamos en memoria dinámica para mostrarlos más a delante de forma ordenada
+ */
+void presencialidad(nodo *head){
+    nodo *headPresencial=NULL, *finalPresencial=NULL, *headHyflex=NULL, *finalHyflex=NULL;
+    nodo *aux=head;
+    int desdeHyflex=0, desdePresencial=0;
+    do{
+        nodo
+        if(aux->hyflex){
+            desdeHyflex++;
+            eQvirtual(headHyflex,finalHyflex,aux2);
+        }
+        else{
+            desdePresencial++;
+            eQpresencial(headPresencial,finalPresencial,aux2);
+        }
+        aux=aux->sgt;
+    }while(aux!=NULL);
+
+    cout<<">Asistentes via presencial: "<<desdePresencial<<endl;
+    cout<<">Asistentes via virtual: "<<desdeHyflex<<endl;
+    cout<<endl<<">ASISTENTES PRESENCIAL:"<<endl;
+    aux=headPresencial;
+    do{
+        cout<<aux->nombre<<" "<<aux->apellidos;
+        aux=aux->sgt;
+    }while(aux!=NULL);
+}
 
 int main() {
     cout << "ACTIVIDAD 2 - DIEGO MARTIN CAMPOS" <<endl;
@@ -191,6 +253,8 @@ int main() {
                 entrega(head);
                 break;
             case 6:
+                cout<<"6. Numero y listado de asistentes a clase Hyflex y presencial"<<endl;
+                presencialidad(head);
                 break;
             case 0:
                 cout <<">Saliendo..."<<endl;
