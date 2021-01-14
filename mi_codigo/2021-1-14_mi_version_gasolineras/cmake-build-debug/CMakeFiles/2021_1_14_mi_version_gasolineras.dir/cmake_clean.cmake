917f63223c079cfcf3f0c440dfdc101327073203
Diego Martin Campos <campos.martin.diego@gmail.com>
2021-01-14 21:12:53 +0100

    Creado mi ejemplo

---
+++ mi_codigo/2021-1-14_mi_version_gasolineras/cmake-build-debug/CMakeFiles/2021_1_14_mi_version_gasolineras.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2021_1_14_mi_version_gasolineras.exe"
+  "2021_1_14_mi_version_gasolineras.exe.manifest"
+  "2021_1_14_mi_version_gasolineras.pdb"
+  "CMakeFiles/2021_1_14_mi_version_gasolineras.dir/main.cpp.obj"
+  "lib2021_1_14_mi_version_gasolineras.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2021_1_14_mi_version_gasolineras.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
